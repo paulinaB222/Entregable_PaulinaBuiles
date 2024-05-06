da7805329c3a1ee68026972e00ee9367707b23f3
paulinaB222 <147001467+paulinaB222@users.noreply.github.com>
2024-05-06 17:58:45 -0500

    Add files via upload

---
+++ Entregable2.py/entregable2.py
@@ -0,0 +1,14 @@
+user="paulina@gmail"
+key=111
+
+
+usuario= input("Usuario")
+
+clave= int(input("clave"))
+
+
+
+if usuario==user==clave==key: 
+ print("Su informacion es correcta")
+else:
+    print("verifique sus datos ")
